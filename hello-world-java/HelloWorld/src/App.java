19194219c2a74e9455716ab8f83ac9d93a77253e
devthiago <devthmello@gmail.com>
2023-09-18 20:03:46 -0300

    My first java project deposited in Git

---
+++ hello-world-java/HelloWorld/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+/* My name is Thiago, a software engineering student 
+at the moment I write this silly code, good luck to everyone
+and Byte. */
